764609c04781b86f71f61b2cef39668612bf9fa1
koynaarya <aryakoyna@gmail.com>
2026-04-01 09:13:29 +0530

    Initialize Groq client and embedding model

--- cli.py
+++ cli.py
@@ -5,7 +5,14 @@ from src.embedding import TranscriptEmbedder
 from src.retrieval import get_top_k
 
 def main():
-    pass
-
+     # 1. Verification of the API Key
+    api_key = "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"
+    client = Groq(api_key=api_key)
+    
+    # 2. Embedding Model Setup
+    print("[*] Initializing embedding model ('all-MiniLM-L6-v2')...")
+    embedder = TranscriptEmbedder()
+    
+   
 if __name__ == "__main__":
     main()
